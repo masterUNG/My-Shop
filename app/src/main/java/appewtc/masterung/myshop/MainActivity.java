e484dc4f152a5fd268f8d3000d81af83438c2df1
masterUNG <phrombutr@gmail.com>
2016-04-14 23:10:32 +0700

    Check Space

--- app/src/main/java/appewtc/masterung/myshop/MainActivity.java
+++ app/src/main/java/appewtc/masterung/myshop/MainActivity.java
@@ -4,18 +4,45 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private EditText userEditText, passwordEditText;
+    private String userString, passwordString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Bind Widget
+        userEditText = (EditText) findViewById(R.id.editText7);
+        passwordEditText = (EditText) findViewById(R.id.editText8);
+
     }   // onCreate
+
+    public void clickSignInMain(View view) {
+
+        userString = userEditText.getText().toString().trim();
+        passwordString = userEditText.getText().toString().trim();
+
+        //Check Space
+        if (userString.equals("") || passwordString.equals("")) {
+            //Have Space
+            MyAlert myAlert = new MyAlert();
+            myAlert.myDialog(this, "มีช่องว่าง",
+                    "กรุณากรอกทุกช่อง คะ");
+        } else {
+            //No Space
+        }
+
+    }   // clickSignIn
 
     public void clickSignUpMain(View view) {
         startActivity(new Intent(MainActivity.this,
                 SignUpActivity.class));
-    }
+    }   // Main Method
 
 }  // Main Class
